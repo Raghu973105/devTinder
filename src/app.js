03607febc7cd4fda3b72a6a6e2cf4b2b18c1575b
rank-rordev <support@rranktec.com>
2025-05-12 19:53:07 +0530

    exploring get,post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// this will only handle GET call to /users/
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Raghu", lastName: "Software Developer"});
+});
+
+// this will match all the HTTP API calls ti /test
 app.use((req, res) => {
     res.send("Hello from server");
 });
