a7cff6b3ecac5f9da6c49e516eb58b61f86be803
rank-rordev <support@rranktec.com>
2025-05-16 01:11:01 +0530

    Password Encrypt

--- src/app.js
+++ src/app.js
@@ -2,18 +2,48 @@ const express = require('express');
 const connectDB =  require("./config/database");
 const User = require('./models/user');
 const app = express();
+const {validateSignUpData} = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req,res) => {
-    // creating a new instance of the user model
-    const user = new User (req.body);
-
     try {
+    // Validation of data
+    validateSignUpData(req);
+    const { firstName,lastName,emailId,password} = req.body;
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    // creating a new instance of the user model
+    const user = new User ({
+        firstName,lastName,emailId,password: passwordHash,
+    });
         await user.save();
         res.send("user added successfully");
     } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message);
+        res.status(400).send("Error : " + err.message);
+    }
+});
+
+app.post("/login", async (req,res) => {
+    try {
+      const { emailId, password } = req.body;
+      
+      const user = await User.findOne({ emailId: emailId});
+      if (!user) {
+        throw new Error("Invalid Credentials");
+      }
+      const isPasswordValid = await bcrypt.compare(password)
+      if (isPasswordValid) {
+        res.send("Login Successful!!")
+      }
+      else {
+        throw new Error("Invalid Credentials");
+      }
+    } catch (err) {
+        res.status(400).send("Error : " + err.message);
     }
 });
 
