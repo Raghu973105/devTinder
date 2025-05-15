5d1e39d66e0d7c8d232840d44d6d2fe73d9f4065
rank-rordev <support@rranktec.com>
2025-05-15 16:34:02 +0530

    Api Level Validation

--- src/app.js
+++ src/app.js
@@ -3,21 +3,87 @@ const connectDB =  require("./config/database");
 const User = require('./models/user');
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req,res) => {
     // creating a new instance of the user model
-    const user = new User ({
-        firstName: "Virat",
-        lastName: "Kholi",
-        emailId: "vk@gmail.com",
-        password: "vk@123"
-
-    });
+    const user = new User (req.body);
 
     try {
         await user.save();
         res.send("user added successfully");
     } catch (err) {
         res.status(400).send("Error saving the user:" + err.message);
+    }
+});
+
+// Get user by email
+app.get("/user", async (req,res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({ emailId: userEmail});
+        if(users.length === 0) {
+            res.status(404).send("User not found");
+        }
+        else {
+         res.send(users)
+        }
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req,res) => {
+
+    try {
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
+    }
+});
+
+//Delete a user from database
+app.delete("/user", async (req,res) => {
+    const userId = req.body.userId;
+    try{
+        //const user = await User.findByIdAndDelete({_id: userId});
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User Deleted Successfully");
+    }
+    catch (err){
+        res.status(400).send("Something went wrong");
+    }
+});
+
+//Update data of the user
+app.patch("/user/:userId", async (req,res) => {
+    const userId = req.params?.userId;
+    const data = req.body;
+    try{
+    const  ALLOWED_UPDATES = ["userId","photoUrl","about","gender","age","skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) => 
+        ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+        throw new Error("update not allowed");
+    }
+    if (data?.skills.length > 10) {
+        throw new Error ("Skills cannot e more than 10");
+    }
+        //const user = await User.findByIdAndDelete({_id: userId});
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true,
+        });
+        res.send("User Updated Successfully");
+    }
+    catch (err){
+        res.status(400).send("Update Failed:" + err.message);
     }
 });
 
